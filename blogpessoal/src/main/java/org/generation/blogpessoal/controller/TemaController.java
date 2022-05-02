d8cfed14222f70a1771c04dd12358f61cdeeb167
unknown <taisanjostata@gmail.com>
2022-05-02 15:01:19 -0300

    Projeto Blog Pessoal

---
+++ blogpessoal/src/main/java/org/generation/blogpessoal/controller/TemaController.java
@@ -0,0 +1,61 @@
+package org.generation.blogpessoal.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.generation.blogpessoal.model.Postagem;
+import org.generation.blogpessoal.model.Tema;
+import org.generation.blogpessoal.repository.TemaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/temas")
+@CrossOrigin ("*")
+public class TemaController {
+
+	@Autowired
+	private TemaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity <List <Tema>> GetAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping ("/{id}")
+	public ResponseEntity<Tema> GetById (@PathVariable long id) {
+	return repository.findById(id)
+	.map(resp ->  ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping ("/titulo/{titulo}")
+	public ResponseEntity <List <Tema>> GetByDescricao(@PathVariable String tema) {
+	return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(tema));
+	}
+	
+	@PostMapping
+	public ResponseEntity <Tema> Post (@Valid @RequestBody Tema tema) {
+	return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
+	}
+	
+	@PutMapping
+	public ResponseEntity <Tema> Put (@RequestBody Tema tema) {
+	return ResponseEntity.status(HttpStatus.OK).body(repository.save(tema));
+	}
+	
+	@DeleteMapping ("/{id}") 
+	public void delete (@PathVariable Long id) {
+	repository.deleteById(id);
+	}
+}
---
+++ blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,82 @@
+package org.generation.blogpessoal.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name= "tb_postagem")
+public class Postagem {
+	
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+@NotNull
+@Size(min = 5, max = 100, message= "O titulo deve ter no minímo 5 caracteres e no máximo 100")
+	private String titulo;
+
+@NotNull
+@Size(min = 10, max = 500, message= "O texto deve ter no minímo 10 caracteres e no máximo 500")
+	private String texto;
+
+@NotNull
+@Temporal (TemporalType.TIMESTAMP)
+private Date date = new java.sql.Date(System.currentTimeMillis());
+
+@ManyToOne
+@JsonIgnoreProperties("postagem")
+private Tema tema;
+
+
+public Tema getTema() {
+	return tema;
+}
+
+public void setTema(Tema tema) {
+	this.tema = tema;
+}
+
+public Long getId() {
+	return id;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+public String getTitulo() {
+	return titulo;
+}
+
+public void setTitulo(String titulo) {
+	this.titulo = titulo;
+}
+
+public String getTexto() {
+	return texto;
+}
+
+public void setTexto(String texto) {
+	this.texto = texto;
+}
+
+public Date getDate() {
+	return date;
+}
+
+public void setDate(Date date) {
+	this.date = date;
+}
+}
